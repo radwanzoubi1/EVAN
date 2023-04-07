bfd55938ba6847e46844b0be77724ff12e550e28
marwanharahsha <80009843+marwanharahsha@users.noreply.github.com>
2023-04-07 04:21:59 +0300

    marwan

---
+++ app/src/main/java/com/example/myapplication/Splash_Screen.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(2500);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class) ;
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
